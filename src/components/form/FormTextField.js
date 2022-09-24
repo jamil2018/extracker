312e7ea06d6615253b39ae3b4b9684f7aba3658c
Hasnat Jamil <hasnatjamil2018@gmail.com>
2022-09-25 00:49:08 +0600

    changeset: implement markup for login page

---
+++ src/components/form/FormTextField.js
@@ -0,0 +1,33 @@
+import { Box, TextField, Typography, useTheme } from "@mui/material";
+import { red } from "@mui/material/colors";
+import { useField } from "formik";
+
+const FormTextField = ({ label, ...props }) => {
+  const theme = useTheme();
+  const [field, meta] = useField(props);
+  return (
+    <Box sx={{ marginBottom: theme.spacing(0.7) }}>
+      <TextField
+        label={label}
+        {...field}
+        {...props}
+        error={meta.touched && meta.error !== null}
+      />
+      {meta.touched && meta.error ? (
+        <Typography variant="subtitle" color={red[400]}>
+          {meta.error}
+        </Typography>
+      ) : (
+        <Typography
+          sx={{ visibility: "hidden" }}
+          variant="caption"
+          color={red[300]}
+        >
+          Error message
+        </Typography>
+      )}
+    </Box>
+  );
+};
+
+export default FormTextField;
--- src/pages/general/Login.js
+++ src/pages/general/Login.js
@@ -1,7 +1,78 @@
+import {
+  Button,
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
+import FormTextField from "../../components/form/FormTextField";
+
 const Login = () => {
+  const theme = useTheme();
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+  const validationSchema = Yup.object({
+    username: Yup.string().required("This field is required"),
+    password: Yup.string().required("This field is required"),
+  });
   return (
     <>
-      <h1>Login Page</h1>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        sx={{
+          height: "80vh",
+        }}
+      >
+        <Card variant="outlined" sx={{ width: "20vw" }}>
+          <CardContent>
+            <Typography
+              variant="h5"
+              component="h1"
+              textAlign="center"
+              gutterBottom
+            >
+              Login
+            </Typography>
+            <Divider
+              sx={{
+                marginBottom: theme.spacing(3),
+              }}
+            />
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+            >
+              <Form>
+                <FormTextField
+                  label="Username"
+                  name="username"
+                  type="text"
+                  size="small"
+                  fullWidth
+                />
+                <FormTextField
+                  label="Password"
+                  name="password"
+                  type="password"
+                  size="small"
+                  fullWidth
+                />
+                <Button variant="contained" type="submit" fullWidth>
+                  Submit
+                </Button>
+              </Form>
+            </Formik>
+          </CardContent>
+        </Card>
+      </Grid>
     </>
   );
 };
