91c611814f9602c866c7b17b875ff0d8936d4341
Hasnat Jamil <hasnatjamil2018@gmail.com>
2022-10-22 02:38:47 +0600

    implement user profile info card

---
+++ src/components/informationDisplay/GridData.js
@@ -0,0 +1,36 @@
+import { Grid, Typography, useTheme } from "@mui/material";
+import { Box } from "@mui/system";
+
+const GridData = ({ labels, values, spacing }) => {
+  const theme = useTheme();
+  return (
+    <>
+      <Grid container justifyContent="center" gap={spacing}>
+        <Grid item>
+          <Box>
+            {labels.map((label) => (
+              <Typography
+                variant="subtitle1"
+                textAlign="left"
+                sx={{ fontWeight: theme.typography.fontWeightBold }}
+              >
+                {label}
+              </Typography>
+            ))}
+          </Box>
+        </Grid>
+        <Grid item>
+          <Box>
+            {values.map((value) => (
+              <Typography variant="subtitle1" textAlign="left">
+                {value}
+              </Typography>
+            ))}
+          </Box>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default GridData;
--- src/pages/user/UserProfile.js
+++ src/pages/user/UserProfile.js
@@ -1,4 +1,13 @@
-import { useTheme } from "@mui/material";
+import {
+  Avatar,
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import GridData from "../../components/informationDisplay/GridData";
 import { Subtitle } from "../../components/text/title/Subtitle";
 import PageHeader from "../../components/userPage/PageHeader";
 
@@ -6,14 +15,47 @@ const UserProfile = () => {
   const theme = useTheme();
   return (
     <>
-      <PageHeader title="Profile" />
+      <PageHeader title="Dashboard" />
       <Subtitle
         sx={{ marginTop: theme.spacing(2) }}
         gutterBottom
         variant="subtitle1"
       >
-        Welcome user
+        Welcome user...
       </Subtitle>
+      <Grid container sx={{ marginTop: theme.spacing(6) }}>
+        <Grid item xs={4}>
+          <Card>
+            <CardContent>
+              <Grid container justifyContent="center">
+                <Avatar
+                  sx={{ width: theme.spacing(18), height: theme.spacing(18) }}
+                >
+                  JD
+                </Avatar>
+              </Grid>
+              <Typography
+                sx={{ marginY: theme.spacing(2) }}
+                textAlign="center"
+                variant="h5"
+              >
+                John Doe
+              </Typography>
+              <Divider
+                sx={{
+                  marginBottom: theme.spacing(2),
+                  marginTop: theme.spacing(4),
+                }}
+              />
+              <GridData
+                labels={["Designation", "Team"]}
+                values={["Software Engineer", "Google"]}
+                spacing={6}
+              />
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </>
   );
 };
