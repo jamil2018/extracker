1472c89b29c0de0ad44da1be7d4d4aad397ebf78
Hasnat Jamil <hasnatjamil2018@gmail.com>
2022-10-24 23:40:59 +0600

    changeset: implement the daily timesheet page

---
+++ src/components/informationDisplay/DailyTimesheetEntryCard.js
@@ -0,0 +1,79 @@
+import {
+  Card,
+  CardContent,
+  Grid,
+  IconButton,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+
+const DailyTimesheetEntryCard = ({
+  id,
+  fromTime,
+  toTime,
+  description,
+  editActionHandler,
+  deleteActionHandler,
+}) => {
+  const theme = useTheme();
+  return (
+    <Grid
+      container
+      justifyContent="space-around"
+      alignItems="center"
+      sx={{ marginY: theme.spacing(3) }}
+    >
+      <Grid container xs={2}>
+        <AccessTimeIcon sx={{ marginRight: theme.spacing(0.5) }} />
+        <Typography>
+          {fromTime} - {toTime}
+        </Typography>
+      </Grid>
+      <Grid container xs={10}>
+        <Card
+          sx={{
+            width: "100%",
+            backgroundColor: theme.palette.primary.main,
+            color: theme.palette.getContrastText(theme.palette.primary.main),
+          }}
+        >
+          <CardContent>
+            <Grid container justifyContent="space-around" alignItems="center">
+              <Grid item xs={9}>
+                {description}
+              </Grid>
+              <Grid container xs={3} justifyContent="flex-end">
+                <IconButton
+                  sx={{ marginRight: theme.spacing(2) }}
+                  onClick={() => editActionHandler(id)}
+                >
+                  <EditIcon
+                    sx={{
+                      color: theme.palette.getContrastText(
+                        theme.palette.primary.main
+                      ),
+                    }}
+                  />
+                </IconButton>
+                <IconButton onClick={() => deleteActionHandler(id)}>
+                  <DeleteIcon
+                    sx={{
+                      color: theme.palette.getContrastText(
+                        theme.palette.primary.main
+                      ),
+                    }}
+                  />
+                </IconButton>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default DailyTimesheetEntryCard;
--- src/pages/user/timesheet/UserDailyTimesheetView.js
+++ src/pages/user/timesheet/UserDailyTimesheetView.js
@@ -1,5 +1,65 @@
+import { Button, Card, CardContent, Grid, useTheme } from "@mui/material";
+import { Subtitle } from "../../../components";
+import DailyTimesheetEntryCard from "../../../components/informationDisplay/DailyTimesheetEntryCard";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+
 const UserDailyTimesheetView = () => {
-  return <>Daily Timesheet</>;
+  const theme = useTheme();
+
+  // action handlers
+  const editHandler = (id) => {
+    console.log("edit button clicked from id: ", id);
+  };
+
+  const deleteHandler = (id) => {
+    console.log("delete button clicked from id: ", id);
+  };
+  return (
+    <>
+      <Grid
+        container
+        justifyContent="space-between"
+        alignItems="center"
+        sx={{ marginBottom: theme.spacing(3) }}
+      >
+        <Subtitle variant="subtitle1">Timesheet Entry For Today</Subtitle>
+        <Button startIcon={<AddCircleOutlineIcon />} variant="outlined">
+          Add New Entry
+        </Button>
+      </Grid>
+      <Card variant="outlined">
+        <CardContent sx={{ paddingX: theme.spacing(6) }}>
+          <DailyTimesheetEntryCard
+            fromTime="10:00 AM"
+            toTime="11:00PM"
+            description="Morning scrum"
+            editActionHandler={(id) => {
+              editHandler(id);
+            }}
+            deleteActionHandler={(id) => deleteHandler(id)}
+          />
+          <DailyTimesheetEntryCard
+            fromTime="11:00 AM"
+            toTime="12:00PM"
+            description="Meeting with product"
+            editActionHandler={(id) => {
+              editHandler(id);
+            }}
+            deleteActionHandler={(id) => deleteHandler(id)}
+          />
+          <DailyTimesheetEntryCard
+            fromTime="12:00 PM"
+            toTime="03:00PM"
+            description="Develop feature X"
+            editActionHandler={(id) => {
+              editHandler(id);
+            }}
+            deleteActionHandler={(id) => deleteHandler(id)}
+          />
+        </CardContent>
+      </Card>
+    </>
+  );
 };
 
 export default UserDailyTimesheetView;
